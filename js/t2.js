aeb011418506d980c645d0773d75f70b8abf476d
Felipe Barbosa <felipebinfo@gmail.com>
2018-05-15 17:27:03 -0300

    Add files via upload

---
+++ js/t2.js
@@ -0,0 +1,27 @@
+pt = {
+	
+	'quem_somos' : 'Quem somos',
+	'quem_somos_card' : 'O projeto GetInShape foi iniciado através da disciplina de Projeto de Desenvolvimento de Sistema Web, do curso de Tecnologia em Análise e Desenvolvimento de Sistemas (TADS) 2017.2'
+	'quem_somos_card2' : 'Os fundadores do projeto são:',
+	'projeto' : 'Projeto',
+	'visite' : 'Visite',
+	'fala_conosco_card' : 'Preencha o formulário abaixo:',
+	'nome' : 'Nome',
+	'email' : 'Email',
+	'mensagem' : 'Mensagem',
+	'enviar' : 'Enviar'
+}
+
+pt = {
+	
+	'quem_somos' : 'Quem somos',
+	'quem_somos_card' : 'The GetInShape project was started through the Web System Development Project discipline of the Systems Analysis and Development Technology (TADS) course 2017.2'
+	'quem_somos_card2' : 'The founders of the project are:',
+	'projeto' : 'Project',
+	'visite' : 'Visit',
+	'fala_conosco_card' : 'Fill out the form below:',
+	'nome' : 'Name',
+	'email' : 'Email',
+	'mensagem' : 'Message',
+	'enviar' : 'Send'
+}
